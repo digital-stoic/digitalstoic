9188ef760c6b32e1544a4b0573e32161d24069b4
mat <mat@digitalstoic.io>
2022-03-09 21:16:35 +0800

    v1.1

--- decks/developers-are-designers/run.sh
+++ decks/developers-are-designers/run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 OUT="out"
+rm -rf $OUT
 mkdir -p $OUT
+cp -Rp img $OUT
 
-npx @marp-team/marp-cli@latest --html --allow-local-files index.md -o ${OUT}/index.html
-npx @marp-team/marp-cli@latest --html --allow-local-files index.md -o ${OUT}/index.pdf
-
-if [ "$DEBUG" = "1" ]; then
+if [ "$DEV" = "1" ]; then
+    npx @marp-team/marp-cli@latest --html --allow-local-files --watch --output ${OUT}/index.html index.md &
     python3 -m http.server 8000
+else
+    npx @marp-team/marp-cli@latest --html --allow-local-files --output ${OUT}/index.html index.md
+    npx @marp-team/marp-cli@latest --html --allow-local-files --output ${OUT}/index.pdf index.md
 fi
